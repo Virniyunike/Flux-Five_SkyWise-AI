ad964305a7124e49b7a1c8291385116c7c76e837
Virniyunike <virniyunike3@gmail.com>
2026-06-23 00:09:36 +0700

    Update settings.py

--- skywise/settings.py
+++ skywise/settings.py
@@ -44,7 +44,15 @@ ROOT_URLCONF = 'skywise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-@@ -48,53 +47,53 @@
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
         },
     },
 ]
