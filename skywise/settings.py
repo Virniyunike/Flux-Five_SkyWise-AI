92a669e0c35522b27f5050acd4310ab565350d26
Virniyunike <virniyunike3@gmail.com>
2026-06-23 01:06:53 +0700

    Update settings.py

--- skywise/settings.py
+++ skywise/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'skywise.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQLDATABASE'),
+        'NAME': os.getenv('MYSQL_DATABASE'),
         'USER': os.getenv('MYSQLUSER'),
         'PASSWORD': os.getenv('MYSQLPASSWORD'),
         'HOST': os.getenv('MYSQLHOST'),
